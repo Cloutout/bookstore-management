3cc586651424b59796afd43f587871bab56152f4
Cloutout <75390041+Cloutout@users.noreply.github.com>
2025-01-01 19:03:21 +0300

    Implement AuthService with login functionality

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && await bcrypt.compare(pass, user.password)) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload: JwtPayload = { username: user.username, roles: user.roles };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
