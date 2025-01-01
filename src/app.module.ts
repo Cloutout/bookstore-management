b284816d13a47d23c5ec0466d32e2dc2e5345b87
Cloutout <75390041+Cloutout@users.noreply.github.com>
2025-01-01 18:15:19 +0300

    Configure TypeORM with PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+// src/app.module.ts
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { BooksModule } from './books/books.module';
+import { BookstoresModule } from './bookstores/bookstores.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres', 
+      password: '12345', 
+      database: 'bookstore_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, 
+    }),
+    AuthModule,
+    BooksModule,
+    BookstoresModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
