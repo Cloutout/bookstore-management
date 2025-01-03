c7f1383530ea355787b728c95ec6e2f3ad76d746
Cloutout <75390041+Cloutout@users.noreply.github.com>
2025-01-03 12:50:52 +0300

    Update user seed passwords to use plain text for development purposes

--- dist/user/user.seed.js
+++ dist/user/user.seed.js
@@ -5,19 +5,19 @@ exports.usersSeed = [
     {
         name: 'User 1',
         email: 'user@user.com',
-        password: '$2b$10$OohblTkrJ1CkLME/g9b4muj.DAICa/16wTtHFmkVhBFzC..imT8PS',
+        password: 'Password123!',
         role: 'user',
     },
     {
         name: 'Admin 1',
         email: 'admin@admin.com',
-        password: '$2b$10$OohblTkrJ1CkLME/g9b4muj.DAICa/16wTtHFmkVhBFzC..imT8PS',
+        password: 'Password123!',
         role: 'admin',
     },
     {
         name: 'Store Manager 1',
         email: 'storeManager@manager.com',
-        password: '$2b$10$OohblTkrJ1CkLME/g9b4muj.DAICa/16wTtHFmkVhBFzC..imT8PS',
+        password: 'Password123!',
         role: 'storeManager',
     },
 ];
